f8a4c977fdec187a39b7d49142a73fc4bc34c1ae
geovanaAnani <geovana.ananias04@gmail.com>
2023-04-21 03:16:30 -0300

    Exercicios

---
+++ Aula 03/calculadoraRacao.js
@@ -0,0 +1,19 @@
+let button = document.getElementById("calcular");
+
+
+button.addEventListener("mouseout", mudarcor)
+button.addEventListener("click", calcular);
+
+let resultado2 = document.getElementById("resultado");
+
+function calcular(){
+    console.log("oi");
+    resultado2.innerHTML = "oi";
+
+}
+
+function mudarcor(){
+    
+}
+
+
---
+++ Aula 03/script.js
@@ -0,0 +1,30 @@
+let numA = 2000;
+let numB = 300;
+let numC = 40;
+
+let resultado = numA > numB ? "eh maior" : "eh menor"
+
+// (validacao) ? (se verdadeiro) : (se falso)
+
+console.log(resultado);
+
+if (numA > numB) {
+    if (numA > numC) {
+        console.log(numA + "eh o maior")
+        if (numA % 2 != 0) {
+            console.log(numA + " eh par")
+        }
+    }
+}
+else if (numB > numA && numB > numC) {
+    console.log(numB + " eh o maior")
+} else {
+    console.log(numC + " eh o maior")
+}
+
+
+for (let i = 0; i<20; i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
