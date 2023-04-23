0189476de7758ed33696dfe0564ee6c8a63f04a6
geovanaAnani <geovana.ananias04@gmail.com>
2023-04-23 10:08:43 -0300

    Atualizando

--- Aula 03/calculadoraRacao.js
+++ Aula 03/calculadoraRacao.js
@@ -1,19 +1,17 @@
 let button = document.getElementById("calcular");
 
+let nomeCachorro = document.getElementById("nome").value;
+let peso = document.getElementById("peso").value;
+let elementoResultado  = document.getElementById("resultado")
 
-button.addEventListener("mouseout", mudarcor)
+let qtdRacao = peso*0.01*1000;
+
+elementoResultado.innerHTML = ' ${nomeCachorro} deve ser ${atdRacao}g por dia'
+
+
+
 button.addEventListener("click", calcular);
 
-let resultado2 = document.getElementById("resultado");
 
-function calcular(){
-    console.log("oi");
-    resultado2.innerHTML = "oi";
-
-}
-
-function mudarcor(){
-    
-}
 
 
